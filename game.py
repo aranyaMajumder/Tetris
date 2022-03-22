c31b7488dae4cf21370c751433d38d67d9a853d5
Firemech9764 <54931838+Firemech9764@users.noreply.github.com>
2022-03-22 15:03:03 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import time
+
+from numpy import true_divide
 import characters
 import os
 import random as r
@@ -16,10 +18,10 @@ for i in range(20):# empty rows
 
 def clearblock(board,shape,shapeb,shapec,y,x):
     for i in range(3):
-        if shape[i]=="*":
+        if shape[i]=="*" and y-1>-1:
             board[y-1][x-1+i]=" "
     for i in range(3):
-        if shapeb[i]=="*":
+        if shapeb[i]=="*" and y>-1:
             board[y][x-1+i]=" "   
     for i in range(3):
         if shapec[i]=="*":
@@ -36,7 +38,7 @@ def nextC(board, y, x,shapeb,  shapec):
                 return 1
     for i in range(3):
         if shape[i]=="*" and shapeb[i]!="*":
-            if board[y][x-1+i]=="\u2588" and i!=1:
+            if board[y-1][x-1+i]=="\u2588" and i!=1:
                 return 1
 
 
@@ -70,9 +72,10 @@ def fillblocks(board, y, x, shapeb, shapec, shape, c):
 
 
 
-def refresh(board):
+def refresh(board,score):
     os.system("cls")
-    row = " "*10
+    print(" "*20 +str(score))
+    row = " "*30
     for y in range(20):
         row=" "*10
         for x in range(20):
@@ -82,13 +85,14 @@ def refresh(board):
 
 T = characters.T
 L = characters.L
+score = 0
 while True:    # main game
     for i in range(20):
         if board[0][i]!=" ":
-            refresh(board)
+            refresh(board, score)
             print("Game Over!")
             exit()   
-
+    score+=1
     ex=0    
     spawn_x = r.randint(2,17)
     rand_character = r.randint(1,2)
@@ -101,10 +105,19 @@ while True:    # main game
         shape = L.shape[rand_rotation][0]
         shapeb = L.shape[rand_rotation][1]
         shapec = L.shape[rand_rotation][2]
-    t = int(time.time())
-    for i in range(17):
+    flag = True
+    i=0
+    t = time.time()
+    while True:
         ex=0
-                
+        if i>16:
+            break
+        if time.time()>=t+0.5 or flag==True:
+            board, ex = fillblocks(board,i+1,spawn_x,shapeb,shapec,shape, "n")
+            refresh(board,score)            
+            flag = False
+            t=time.time()
+            i+=1
         if keyboard.is_pressed('a'):
             if board[i][spawn_x-1]==" " or board[i][spawn_x-2]==" ":
                 if board[i+1][spawn_x-2]==" " or board[i+1][spawn_x-2]==" ":
@@ -112,22 +125,19 @@ while True:    # main game
                         board = clearblock(board,shape,shapeb,shapec,i,spawn_x)
                         spawn_x-=1
                         board, ex = fillblocks(board,i, spawn_x, shapeb,shapec,shape, "l")
-                        refresh(board)
+                        refresh(board,score)
+                        
         if keyboard.is_pressed('d'):
-            if board[i][spawn_x+1]==" " or board[i][spawn_x+2]==" ":
-                if board[i+1][spawn_x+2]==" " or board[i+1][spawn_x+2]==" ":
-                    if board[i+2][spawn_x+2]==" " or board[i+2][spawn_x+2]==" ":
-                        board = clearblock(board,shape,shapeb,shapec,i,spawn_x)
-                        spawn_x+=1
-                        board, ex = fillblocks(board,i, spawn_x, shapeb,shapec,shape, "r")
-                        refresh(board)
+            if spawn_x<18:
+                if board[i][spawn_x+1]==" " or board[i][spawn_x+2]==" ":
+                    if board[i+1][spawn_x+2]==" " or board[i+1][spawn_x+2]==" ":
+                        if board[i+2][spawn_x+2]==" " or board[i+2][spawn_x+2]==" ":
+                            board = clearblock(board,shape,shapeb,shapec,i,spawn_x)
+                            spawn_x+=1
+                            board, ex = fillblocks(board,i, spawn_x, shapeb,shapec,shape, "r")
+                            refresh(board,score)
+                            
+         
         
-        
-        board, ex = fillblocks(board,i+1,spawn_x,shapeb,shapec,shape, "n")
-        refresh(board)
-                 
-        time.sleep(0.01)
         if ex == 1:
             break
-
-        
